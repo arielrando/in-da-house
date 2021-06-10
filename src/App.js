fea3eecb80f04803042e9b25deacec1150f86f93
arielrando <ariel_rando@hotmail.com.ar>
2021-06-09 23:55:32 -0300

    mejoras en title y favicon

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 import {BrowserRouter, Switch, Route} from 'react-router-dom'
 
 function App() {
+  document.title = "In Da House";
   return (
     <div className="App">
       <header className="App-header">
